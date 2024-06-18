762ce84c08464fcbd9d11c574dce96c61985f5c9
erichround <erich.r.round@gmail.com>
2024-06-18 22:31:45 +0100

    Added combinatorics functions to stats

--- R/stats.R
+++ R/stats.R
@@ -268,3 +268,145 @@ str2ints = function(s) {
 count_exponents = function(m) {
   apply(m, 2, function(v) { length(unique(v)) })
 }
+
+
+#' Count the number of ways to place p stones in an m x n grid, under the
+#' constraints that no row is empty; no column is empty; only one stone per
+#' cell; and cell (1,1) is filled.
+#' @param m An integer. The number of grid rows.
+#' @param n An integer. The number of grid columns.
+#' @param p An integer. The number of stones
+#' @return An integer. The number of solutions.
+count_arrangements = function(m,n,p) {
+  # Calculate an inclusion-exclusion sum
+  summands <- matrix(0, m, n)
+  for (n_0cols in 0:(n-1)) { # number of empty columns
+    for (n_0rows in 0:(m-1)) { # number of empty rows
+      choose_0cols <- choose(n - 1, n_0cols) # choices of the empty columns
+      choose_0rows <- choose(m - 1, n_0rows) # choices of the empty rows
+      n_available_cols <- n - n_0cols
+      n_available_rows <- m - n_0rows
+      n_available_cells <- n_available_cols * n_available_rows - 1
+      choose_cells = choose(n_available_cells, p - 1)
+      incl_excl_sign <- (-1)^(n_0cols + n_0rows)
+      summands[n_0rows+1, n_0cols+1] <- incl_excl_sign * choose_0rows * choose_0cols * choose_cells
+    }
+  }
+  print(summands)
+  sum(summands)
+}
+
+#' Count the number of ways to place p stones in an m x n grid, under the
+#' constraints that no row is empty; no column is empty; only one stone per
+#' cell; cell (1,1) is filled; and cell (1,j) must also be filled, for some j >
+#' 1. This indicates the how many times a cell (1,j) is filled, among all the
+#' solutions to the grid-filling constraints described for
+#' `count_arrangements()`.
+#' @param m An integer. The number of grid rows.
+#' @param n An integer. The number of grid columns.
+#' @param p An integer. The number of stones
+#' @return An integer. The number of solutions.
+count_arrangements_1j = function(m, n, p) {
+  # Calculate an inclusion-exclusion sum
+  summands <- matrix(0, m, n-1)
+  for (n_0cols in 0:(n-2)) { # number of empty columns
+    for (n_0rows in 0:(m-1)) { # number of empty rows
+      choose_0cols <- choose(n - 2, n_0cols) # choices of the empty columns
+      choose_0rows <- choose(m - 1, n_0rows) # choices of the empty rows
+      n_available_cols <- n - n_0cols
+      n_available_rows <- m - n_0rows
+      n_available_cells <- n_available_cols * n_available_rows - 1
+      choose_cells = choose(n_available_cells, p - 2)
+      incl_excl_sign <- (-1)^(n_0cols + n_0rows)
+      summands[n_0rows+1, n_0cols+1] <- incl_excl_sign * choose_0rows * choose_0cols * choose_cells
+    }
+  }
+  print(summands)
+  sum(summands)
+}
+
+#' Count the number of ways to place p stones in an m x n grid, under the
+#' constraints that no row is empty; no column is empty; only one stone per
+#' cell; cell (1,1) is filled; and cell (i,1) must also be filled, for some i >
+#' 1. This indicates the how many times a cell (i,1) is filled, among all the
+#' solutions to the grid-filling constraints described for
+#' `count_arrangements()`.
+#' @param m An integer. The number of grid rows.
+#' @param n An integer. The number of grid columns.
+#' @param p An integer. The number of stones
+#' @return An integer. The number of solutions.
+count_arrangements_i1 = function(m, n, p) {
+  # Calculate an inclusion-exclusion sum
+  summands <- matrix(0, m-1, n)
+  for (n_0cols in 0:(n-1)) { # number of empty columns
+    for (n_0rows in 0:(m-2)) { # number of empty rows
+      choose_0cols <- choose(n - 1, n_0cols) # choices of the empty columns
+      choose_0rows <- choose(m - 2, n_0rows) # choices of the empty rows
+      n_available_cols <- n - n_0cols
+      n_available_rows <- m - n_0rows
+      n_available_cells <- n_available_cols * n_available_rows - 1
+      choose_cells = choose(n_available_cells, p - 2)
+      incl_excl_sign <- (-1)^(n_0cols + n_0rows)
+      summands[n_0rows+1, n_0cols+1] <- incl_excl_sign * choose_0rows * choose_0cols * choose_cells
+    }
+  }
+  print(summands)
+  sum(summands)
+}
+
+#' Count the number of ways to place p stones in an m x n grid, under the
+#' constraints that no row is empty; no column is empty; only one stone per
+#' cell; cell (1,1) is filled; and cell (i,j) must also be filled, for some i >
+#' 1, j > 1. This indicates the how many times a cell (i,j) is filled, among all
+#' the solutions to the grid-filling constraints described for
+#' `count_arrangements()`.
+#' @param m An integer. The number of grid rows.
+#' @param n An integer. The number of grid columns.
+#' @param p An integer. The number of stones
+#' @return An integer. The number of solutions.
+count_arrangements_ij = function(m, n, p) {
+  # Calculate an inclusion-exclusion sum
+  summands <- matrix(0, m-1, n-1)
+  for (n_0cols in 0:(n-2)) { # number of empty columns
+    for (n_0rows in 0:(m-2)) { # number of empty rows
+      choose_0cols <- choose(n - 2, n_0cols) # choices of the empty columns
+      choose_0rows <- choose(m - 2, n_0rows) # choices of the empty rows
+      n_available_cols <- n - n_0cols
+      n_available_rows <- m - n_0rows
+      n_available_cells <- n_available_cols * n_available_rows - 1
+      choose_cells = choose(n_available_cells, p - 2)
+      incl_excl_sign <- (-1)^(n_0cols + n_0rows)
+      summands[n_0rows+1, n_0cols+1] <- incl_excl_sign * choose_0rows * choose_0cols * choose_cells
+    }
+  }
+  print(summands)
+  sum(summands)
+}
+
+#' Generate the solutions to placing p stones in an m x n grid, under the
+#' constraints that no row is empty; no column is empty; only one stone per
+#' cell; and cell (1,1) is filled.
+#' @param m An integer. The number of grid rows.
+#' @param n An integer. The number of grid columns.
+#' @param p An integer. The number of stones
+#' @return A list of matrices. The solutions, where 1 = filled cell.
+generate_arrangements = function(m, n, p){
+  cell_indices <- 2:(m * n)
+  combos <- combn(cell_indices, p - 1)
+  n_combos <- ncol(combos)
+  matdat <- c(1, rep(0, m * n - 1))
+  solution_list <- list()
+  blanks <- matrix(0, m, n)
+  for (i in 1:n_combos) {
+    matdati <- matdat
+    matdati[combos[,i]] <- 1
+    mati <- matrix(matdati, nrow = m)
+    rblank <- sum(rowSums(mati) == 0)
+    cblank <- sum(colSums(mati) == 0)
+    blanks[rblank + 1, cblank + 1] <- blanks[rblank + 1, cblank + 1] + 1
+    if (rblank == 0 | cblank == 0) { next }
+    solution_list[[length(solution_list) +1]] <- mati
+  }
+  print(blanks)
+  solution_list
+}
